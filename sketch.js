b5bd753dd56226e8a4755d5979b3b1588ccecd91
SpyChicken00 <94644105+SpyChicken00@users.noreply.github.com>
2026-09-05 16:34:41 +0900

    Add hover effect for etch a sketch

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@
 
 const squaresDiv = document.querySelector(".squaresContainer")
 const gridSizeButton = document.querySelector("#gridSizeButton")
+const CUBE_COLOR_DEFAULT = "green"
+const CUBE_COLOR_HOVER = "red"
 
 
 //generate a specific # of square shaped divs in div container
@@ -28,7 +30,23 @@ function removeSquares () {
         squaresDiv.firstChild.remove();
     }
 }
+     
 
+
+//Event Listeners for squares and buttons
+
+//Mouse enter and Mouse exit creates a hover effect
+//add handler to squaresDiv parent, let event bubble to parent
+//mouse enter square, change cube color
+//mouse leave square, change cube color back to
+squaresDiv.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = CUBE_COLOR_HOVER;
+})
+squaresDiv.addEventListener("mouseout", (event) => {
+    setTimeout(function(){
+        event.target.style.backgroundColor = CUBE_COLOR_DEFAULT;
+    }, 2000)
+})
 gridSizeButton.addEventListener("click", (event) => {
     console.log(event.target.id)
     const numOfSquares = prompt("Please enter a number less than 100")
@@ -37,8 +55,5 @@ gridSizeButton.addEventListener("click", (event) => {
     removeSquares();
     generateSquares(numOfSquares)
 })
-
-
-
 
 generateSquares(16)
